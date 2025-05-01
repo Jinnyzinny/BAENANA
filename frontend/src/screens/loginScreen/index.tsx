45b4034a55e7f2030cba781ec7eee4843376ddff
최이화 <ihwa220@naver.com>
2025-05-01 19:26:01 +0900

    feat: 로그인 버튼 추가

--- frontend/src/screens/loginScreen/index.tsx
+++ frontend/src/screens/loginScreen/index.tsx
@@ -1,7 +1,7 @@
-import { Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Button } from "../../components/common/button";
 import { useLoginStore } from "../../store/loginStore";
+import { LoginButton } from "../../components/login/loginButton";
 
 export function LoginScreen() {
   const setLogin = useLoginStore((state) => state.setLogin);
@@ -11,9 +11,16 @@ export function LoginScreen() {
   }
 
   return (
-    <SafeAreaView>
-      <Text>로그인</Text>
-      <Button content="로그인" onPress={handleLogin} fill={true} />
+    <SafeAreaView className="flex-1 bg-neutral-100 mx-5 justify-center gap-20">
+      <Image
+        source={require("../../assets/images/mascot.png")}
+        className="w-32 h-32 self-center"
+      />
+      <View className="gap-5 mx-10">
+        <Text>SNS 계정으로 로그인</Text>
+        <LoginButton type="kakao" onPress={handleLogin} />
+        <LoginButton type="google" onPress={handleLogin} />
+      </View>
     </SafeAreaView>
   );
 }
