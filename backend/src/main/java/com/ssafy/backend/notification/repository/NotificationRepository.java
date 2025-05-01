10b8b2aeb9535b162ce59f107b578193f0d84f92
김민경 <1016mk@naver.com>
2025-05-01 23:17:47 +0900

    feat: Add Notification Repository

--- backend/src/main/java/com/ssafy/backend/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/ssafy/backend/notification/repository/NotificationRepository.java
@@ -1,4 +1,14 @@
 package com.ssafy.backend.notification.repository;
 
-public class NotificationRepository {
+import com.ssafy.backend.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    // 최신순으로 공지사항 목록 조회
+    List<Notification> findAllByOrderByCreatedAtDesc();
 }
