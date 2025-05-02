01a3d1e4d7d2a9d4a82867edf6a6692e0c704b7f
이진형 <ddocdoli@naver.com>
2025-05-02 16:25:08 +0900

    fix : Calendar와 Home 부분 @AuthenticationPrincipal 추가

--- backend/src/main/java/com/ssafy/backend/home/controller/HomeController.java
+++ backend/src/main/java/com/ssafy/backend/home/controller/HomeController.java
@@ -4,9 +4,11 @@ import com.ssafy.backend.home.dto.response.HospitalReservationResDto;
 import com.ssafy.backend.home.dto.response.MedicineResDto;
 import com.ssafy.backend.home.dto.response.RemainDayResDto;
 import com.ssafy.backend.home.service.HomeService;
+import com.ssafy.backend.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,14 +29,14 @@ public class HomeController {
 
     @GetMapping("/alarm/medicine")
     public ResponseEntity<MedicineResDto> getMedicine(
-//            @AuthenticationPrincipal UserDetails userDetails
+            @AuthenticationPrincipal User user
     ) {
         return ResponseEntity.ok(homeService.getMedicine());
     }
 
     @GetMapping("/alarm/reservation")
     public ResponseEntity<HospitalReservationResDto> getHospitalReservation(
-//            @AuthenticationPrincipal UserDetails userDetails
+            @AuthenticationPrincipal User user
     ) {
         return ResponseEntity.ok(homeService.getHospitalReservation());
     }
