9d7b941cbe441681fbc5cbff20f8d6898bed97bc
이진형 <ddocdoli@naver.com>
2025-05-05 12:18:00 +0900

    fix : 생리주기 기능 수정

--- backend/src/main/java/com/ssafy/backend/menstrual/service/report/MenstrualServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/menstrual/service/report/MenstrualServiceImpl.java
@@ -7,6 +7,8 @@ import com.ssafy.backend.report.dto.response.GetAllMenstrualResDto;
 import com.ssafy.backend.report.dto.response.GetMenstrualInfoResDto;
 import com.ssafy.backend.report.dto.response.GetOvulationTestResDto;
 import com.ssafy.backend.report.dto.response.GetRecentMenstrualResDto;
+import com.ssafy.backend.report.dto.service.GetCycleDto;
+import com.ssafy.backend.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,8 +28,8 @@ public class MenstrualServiceImpl implements MenstrualService {
 
 
     @Override
-    public GetMenstrualInfoResDto getMenstrualInfo() {
-        Long userId = 0L;
+    public GetMenstrualInfoResDto getMenstrualInfo(User user) {
+        Long userId = user.getUserId();
         List<MenstrualCycle> menstrualCycleList =
                 menstrualCycleRepository.findByUser_UserId(userId).orElseThrow();
 
@@ -51,14 +53,14 @@ public class MenstrualServiceImpl implements MenstrualService {
                 .cycle(avgCycle)
                 .period(avgPeriod)
 //                정상 판별 어떻게 할 예정??
-//                .is_cycle_normal()
-//                .is_period_normal()
+                .is_cycle_normal(null)
+                .is_period_normal(null)
                 .build();
     }
 
     @Override
-    public GetOvulationTestResDto getOvulationTest() {
-        Long userId = 0L;
+    public GetOvulationTestResDto getOvulationTest(User user) {
+        Long userId = user.getUserId();
 
 //        디아 비전측 정보로 채운다. 이 부분은 제공 API로 대체 가능성 높음
         return GetOvulationTestResDto.builder()
@@ -69,46 +71,35 @@ public class MenstrualServiceImpl implements MenstrualService {
     }
 
     @Override
-    public GetRecentMenstrualResDto getRecentMenstrual() {
-        Long userId = 0L;
+    public GetRecentMenstrualResDto getRecentMenstrual(User user) {
+        Long userId = user.getUserId();
         List<MenstrualCycle> menstrualCycleList =
                 menstrualCycleRepository.findTop6ByUser_UserIdOrderByStartDateDesc(userId).orElseThrow();
 
-        int cycleSum = 0;
-        int maxCycle = Integer.MIN_VALUE;
-        for (int i = 1; i < menstrualCycleList.size(); i++) {
-            LocalDate startDate = menstrualCycleList.get(i).getStartDate();
-            LocalDate prevStartDate = menstrualCycleList.get(i - 1).getStartDate();
-
-            maxCycle = Math.max(maxCycle, (int) ChronoUnit.DAYS.between(prevStartDate, startDate));
-            cycleSum += (int) ChronoUnit.DAYS.between(prevStartDate, startDate);
-        }
-
-        List<GetRecentMenstrualResDto.each_cycle_record> cycleRecord = new ArrayList<>();
-
-        for (MenstrualCycle cycle : menstrualCycleList) {
-            cycleRecord.add(GetRecentMenstrualResDto.each_cycle_record.builder()
-                    .start_date(cycle.getStartDate().toString())
-                    .end_date(cycle.getEndDate().toString())
-                    .period((int) ChronoUnit.DAYS.between(cycle.getStartDate(),cycle.getEndDate()))
-                    .build());
-        }
-        int averageCycle = cycleSum / menstrualCycleList.size();
-
+        GetCycleDto cycle =  getCycleInfo(menstrualCycleList);
         return GetRecentMenstrualResDto.builder()
-                .average_cycle(averageCycle)
-                .max_cycle(maxCycle)
-                .cycle_record(cycleRecord)
+                .average_cycle(cycle.getAverageCycle())
+                .max_cycle(cycle.getMaxCycle())
+                .cycle_record(cycle.getCycleRecord())
                 .build();
     }
 
     @Override
-    public GetAllMenstrualResDto getAllMenstrual() {
-        Long userId = 0L;
+    public GetAllMenstrualResDto getAllMenstrual(User user) {
+        Long userId = user.getUserId();
 
-        List<MenstrualCycle> menstrualCycleList=
+        List<MenstrualCycle> menstrualCycleList =
                 menstrualCycleRepository.findByUser_UserIdOrderByStartDateDesc(userId).orElseThrow();
 
+        GetCycleDto cycle =  getCycleInfo(menstrualCycleList);
+        return GetAllMenstrualResDto.builder()
+                .average_cycle(cycle.getAverageCycle())
+                .max_cycle(cycle.getMaxCycle())
+                .cycle_record(cycle.getCycleRecord())
+                .build();
+    }
+
+    public GetCycleDto getCycleInfo(List<MenstrualCycle> menstrualCycleList) {
         int cycleSum = 0;
         int maxCycle = Integer.MIN_VALUE;
         for (int i = 1; i < menstrualCycleList.size(); i++) {
@@ -125,15 +116,15 @@ public class MenstrualServiceImpl implements MenstrualService {
             cycleRecord.add(GetRecentMenstrualResDto.each_cycle_record.builder()
                     .start_date(cycle.getStartDate().toString())
                     .end_date(cycle.getEndDate().toString())
-                    .period((int) ChronoUnit.DAYS.between(cycle.getStartDate(),cycle.getEndDate()))
+                    .period((int) ChronoUnit.DAYS.between(cycle.getStartDate(), cycle.getEndDate()))
                     .build());
         }
         int averageCycle = cycleSum / menstrualCycleList.size();
 
-        return GetAllMenstrualResDto.builder()
-                .average_cycle(averageCycle)
-                .max_cycle(maxCycle)
-                .cycle_record(cycleRecord)
+        return GetCycleDto.builder()
+                .averageCycle(averageCycle)
+                .maxCycle(maxCycle)
+                .cycleRecord(cycleRecord)
                 .build();
     }
 }
