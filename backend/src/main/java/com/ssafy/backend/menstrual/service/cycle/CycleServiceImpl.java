01ab6dcd1efeedfecac23b799288d77601b65e95
이진형 <ddocdoli@naver.com>
2025-05-15 22:28:36 +0900

    fix :  Detail 삭제

--- backend/src/main/java/com/ssafy/backend/menstrual/service/cycle/CycleServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/menstrual/service/cycle/CycleServiceImpl.java
@@ -67,22 +67,23 @@ public class CycleServiceImpl implements CycleService {
                                         .cycle_id(cycle.getCycleId())
                                         .start_date(cycle.getStartDate().toString())
                                         .end_date(cycle.getEndDate().toString())
-                                        .detail(
-                                                cycle.getLogs().stream().map(
-                                                                log ->
-                                                                        GetMenstrualCycleResDto.SymptomDailyDetail.builder()
-                                                                                .daily_log_id(log.getDailyId())
-                                                                                .date(log.getDate().toString())
-                                                                                .bleeding_level(log.getBleedingLevel())
-                                                                                .pain_level(log.getPainLevel())
-                                                                                .stress_level(log.getStressLevel())
-                                                                                .symptoms(
-                                                                                        log.getSymptomLog().stream().map(
-                                                                                                SymptomLog::getSymptomType
-                                                                                        ).toList()
-                                                                                ).build())
-                                                        .toList()
-                                        ).build()
+//                                        .detail(
+//                                                cycle.getLogs().stream().map(
+//                                                                log ->
+//                                                                        GetMenstrualCycleResDto.SymptomDailyDetail.builder()
+//                                                                                .daily_log_id(log.getDailyId())
+//                                                                                .date(log.getDate().toString())
+//                                                                                .bleeding_level(log.getBleedingLevel())
+//                                                                                .pain_level(log.getPainLevel())
+//                                                                                .stress_level(log.getStressLevel())
+//                                                                                .symptoms(
+//                                                                                        log.getSymptomLog().stream().map(
+//                                                                                                SymptomLog::getSymptomType
+//                                                                                        ).toList()
+//                                                                                ).build())
+//                                                        .toList()
+//                                        )
+                                        .build()
                 ).toList());
     }
 
@@ -99,7 +100,6 @@ public class CycleServiceImpl implements CycleService {
             return ApiResponse.success("해당 월에는 주기 정보가 존재하지 않습니다.");
         }
 
-
         return ApiResponse.success(
                 "월별 생리 주기 정보를 열람한다.",
                 menstrualCycleList.stream().distinct().map(
@@ -108,24 +108,24 @@ public class CycleServiceImpl implements CycleService {
                                                 .cycle_id(cycle.getCycleId())
                                                 .start_date(cycle.getStartDate().toString())
                                                 .end_date(cycle.getEndDate().toString())
-                                                .detail(
-                                                        cycle.getLogs().stream().distinct().map(
-                                                                log ->
-                                                                        GetMenstrualCycleResDto.SymptomDailyDetail.builder()
-                                                                                .daily_log_id(log.getDailyId())
-                                                                                .date(log.getDate().toString())
-                                                                                .bleeding_level(log.getBleedingLevel())
-                                                                                .pain_level(log.getPainLevel())
-                                                                                .stress_level(log.getStressLevel())
-                                                                                .symptoms(
-                                                                                        log.getSymptomLog().stream().distinct().map(
-                                                                                                SymptomLog::getSymptomType
-                                                                                        ).toList()
-                                                                                )
-
-                                                                                .build()
-                                                        ).toList()
-                                                )
+//                                                .detail(
+//                                                        cycle.getLogs().stream().distinct().map(
+//                                                                log ->
+//                                                                        GetMenstrualCycleResDto.SymptomDailyDetail.builder()
+//                                                                                .daily_log_id(log.getDailyId())
+//                                                                                .date(log.getDate().toString())
+//                                                                                .bleeding_level(log.getBleedingLevel())
+//                                                                                .pain_level(log.getPainLevel())
+//                                                                                .stress_level(log.getStressLevel())
+//                                                                                .symptoms(
+//                                                                                        log.getSymptomLog().stream().distinct().map(
+//                                                                                                SymptomLog::getSymptomType
+//                                                                                        ).toList()
+//                                                                                )
+//
+//                                                                                .build()
+//                                                        ).toList()
+//                                                )
                                                 .build()
                         )
                         .toList()
